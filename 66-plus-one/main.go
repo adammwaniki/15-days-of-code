d4b681e8e8891b1601170828afc43f270af450bd
Adam Mwaniki <adamndegwa@protonmail.com>
2025-03-20 17:59:15 +0300

    Work in Progress

--- 66-plus-one/main.go
+++ 66-plus-one/main.go
@@ -1,8 +1,20 @@
 package main
 
+import "strconv"
+
 func plusOne(digits []int) []int {
     var ans int = (digits[len(digits)-1] + 1)
-    digits[len(digits)-1] = ans
+    
+	if ans > 9 {
+		numStr := strconv.Itoa(ans)
+		for _, v := range numStr {
+			// Since int(v) directly converts a rune (character) to its ASCII value
+			// subtract '0' to get the integer value
+			digits = append(digits, int(v - '0'))
+		}
+	} else {
+		digits[len(digits)-1] = ans
+	}
     return digits
 }
 
