299be0222cf6f1d8b125ccbc845a56b6f596d2b2
Adam Mwaniki <adamndegwa@protonmail.com>
2025-03-27 01:02:36 +0300

    Work in progress

--- 2033-minimum-steps-for-uni-value-grid/main.go
+++ 2033-minimum-steps-for-uni-value-grid/main.go
@@ -1,7 +1,8 @@
 /*
 *Algo:
 - Iterate over the grid so that we can target the slices in a nested loop
-- Iterate over each slice and produce a joined slice e.g using slices.Concat() 
+- Iterate over each slice and produce a joined slice 
+    e.g using slices.Concat() or the variadic operator ...
 - For mathematically impossible cases return -1,
     case1: evaluate for num > x { if num%x != 0 {return -1}}
     case2: evaluate for num < x { if x%num != 0 {return -1}}
@@ -13,7 +14,43 @@
 
 package main
 
+import "slices"
+
 func minOperations(grid [][]int, x int) int {
+    // Handling egde cases where the grid is empty
+    if len(grid) == 0 {
+        return -1 
+    }
+
+    grandLine := grid[0]
+    for _, gridLine := range grid[1:] {
+        grandLine = append(grandLine, gridLine...)
+    } 
+    slices.Sort(grandLine)
+    
+    mid := grandLine[1]
+    steps := 0
+    for _, num := range grandLine {
+        for num > x {
+            ans := num % x
+            if ans != 0 {
+                return -1
+            } else {
+                steps = (num - mid)/x
+                steps += steps
+            }
+        } 
+        for num < x { 
+            ans := x % num
+            if ans != 0 {
+                return -1
+            } else {
+                steps = (num - mid)/x
+                steps += steps
+            }
+        }
+    }
+    return steps
     
 }
 
--- 2033-minimum-steps-for-uni-value-grid/main_test.go
+++ 2033-minimum-steps-for-uni-value-grid/main_test.go
@@ -11,19 +11,23 @@ func TestMinOperations(t *testing.T){
 		x int
 		expected int
 	}{
-		// Test case 1: input [[1,2],[3,4]], output 4
-		{},
+		// Test case 1: input [[2,4],[6,8]], x is 2, output 4
+		{[][]int{
+			{2,4},
+			{6,8},
+			},
+			2, 4},
 		
-		// Test case 2: input [[1,2],[3,4]], output 4
-		{},
+		// Test case 2: input [[1,5],[2,3]], x is 1, output 5
+		{[][]int{{1,5}, {2,3}}, 1, 5},
 
-		// Test case 3: input [[1,2],[3,4]], output 4
-		{},
+		// Test case 3: input [[1,2],[3,4]], x is 2, output -1
+		{[][]int{{1,2}, {3,4}}, 2, -1},
 	}
 	for _, test := range tests {
 		result := minOperations(test.grid, test.x)
 		if !reflect.DeepEqual(test.expected, result){
-			t.Errorf("minOperations(%v, %v) = %v, expected %v", test.grid, test.x, test.expected)
+			t.Errorf("minOperations(%v, %v) = %v, expected %v", test.grid, test.x, result, test.expected)
 		}
 	}
 }
