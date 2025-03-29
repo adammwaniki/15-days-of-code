9dcb97c33d76b0259f2d6d5aa0b07c25b6022cd7
Adam Mwaniki <adamndegwa@protonmail.com>
2025-03-29 23:57:46 +0300

    WIP

--- 118-pascals-triangle/main.go
+++ 118-pascals-triangle/main.go
@@ -1,12 +1,12 @@
 package main
 
-// generatePascalsRow computes Pascal's Triangle row n
-func generate(n int) []int64 {
-	row := make([]int64, n+1)
+// generate computes Pascal's Triangle row n
+func generate(n int) []int {
+	row := make([]int, n+1)
 	row[0] = 1 // First element is always 1
 
 	for k := 1; k <= n; k++ {
-		row[k] = row[k-1] * int64(n-k+1) / int64(k)
+		row[k] = row[k-1] * int(n-k+1) / int(k)
 	}
 
 	return row
