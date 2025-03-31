6699e7194a47da8d2c9349b8ff09992c1605ab42
Adam Mwaniki <160830609+adammwaniki@users.noreply.github.com>
2025-03-31 23:07:12 +0300

    Update main.go comments on option 1 time complexity

--- 125-valid-palindrome/main.go
+++ 125-valid-palindrome/main.go
@@ -5,22 +5,24 @@
 - Join the characters and convert to lowercase to be case insensitive
 - A string is a slice of bytes
 - Option 1: Iterate from both ends of the string and compare iterators
-    - for i, j := 0, len(s)-1 ; i < j ; i, j = i+1, j-1 {
+    - for i, j, mid := 0, len(s)-1, int((i+j)/2) ; i < mid && j > mid ; i, j = i+1, j-1 {
         if s[i] != s[j] {
             return true
         } else {
             return false
         }
     }
+    O(n) time, brought it down from O(n²)
 - Option 2: split the string in half and compare the values e.g. using reflect.DeepEqual
     - high := len(s) - 1
     - low := 0
     - mid := int((high + low)/2) // force it to round down
-    if !reflect.DeepEqual(s[low:mid], s[mid:high]) {
+    if !reflect.DeepEqual(s[low:mid], s[mid:high]) { // reflect.DeepEqual is ill advised in production 
         return false
     } else {
         return true
     }
+    - Find a way to improve time complexity 
 */
 package main
 
