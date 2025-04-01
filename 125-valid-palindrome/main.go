2bd86d81f425d76a34bd65e9aab4e5310e21b7ec
Adam Mwaniki <adamndegwa@protonmail.com>
2025-04-01 13:36:34 +0300

    Passes tests pending refactoring

--- 125-valid-palindrome/main.go
+++ 125-valid-palindrome/main.go
@@ -6,32 +6,48 @@
 - This can be handled using the regexp package
 - A string is a slice of bytes
 - Option 1: Iterate from both ends of the string and compare iterators
-    - for i, j, mid := 0, len(s)-1, int((i+j)/2) ; i < mid && j > mid ; i, j = i+1, j-1 {
-        if s[i] != s[j] {
-            return true
-        } else {
-            return false
-        }
+  - for i, j, mid := 0, len(s)-1, int((i+j)/2) ; i < mid && j > mid ; i, j = i+1, j-1 {
+    if s[i] != s[j] {
+    return true
+    } else {
+    return false
+    }
     }
     O(n) time, brought it down from O(n²)
+
 - Option 2: split the string in half and compare the values e.g. using reflect.DeepEqual
-    - high := len(s) - 1
-    - low := 0
-    - mid := int((high + low)/2) // force it to round down
-    if !reflect.DeepEqual(s[low:mid], s[mid:high]) { // reflect.DeepEqual is ill advised in production 
-        return false
+  - high := len(s) - 1
+  - low := 0
+  - mid := int((high + low)/2) // force it to round down
+    if !reflect.DeepEqual(s[low:mid], s[mid:high]) { // reflect.DeepEqual is ill advised in production
+    return false
     } else {
-        return true
+    return true
     }
-    - Find a way to improve time complexity 
+  - Find a way to improve time complexity
 */
 package main
 
-func isPalindrome(s string) bool {
-    if s == " " {
-        return true
-    }
+import "regexp"
 
+func isPalindrome(s string) bool {
+    // Early return true if length of string is 1
+    if len([]rune(s)) == 1 {
+        return true
+    } 
+
+    pattern := regexp.MustCompile(`[^,.!@#$%^&\s]`) // regex negated character set denoted by ^
+    matches := pattern.FindAllString(s, -1)
+
+    i, j  := 0, len(matches)-1
+    mid := int((i+j)/2)
+
+    for  ; i < mid && j > mid ; i, j = i+1, j-1 {
+        if matches[i] != matches[j] {
+            return true
+        }
+    }
+    return false
 }
 
 func main(){}
