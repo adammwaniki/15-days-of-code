08dc761bebb5b68a2664db6dc7b825185ce151cd
Adam Mwaniki <adamndegwa@protonmail.com>
2025-04-02 00:57:13 +0300

    Completes task and unit tests

--- 141-linked-list-cycle/main.go
+++ 141-linked-list-cycle/main.go
@@ -1,10 +1,56 @@
 package main
 
+/*
+* Algo:
+ - Apply Floyd's Tortoise and Hare Algorithm or Brent's algorithm
+ - Option 1: Tortoise and Hare
+ - The algorithm maintains two pointers into the given sequence
+ - One (the tortoise) at x_i, and the other (the hare) at x_2i
+ - At each step of the algorithm, it moves i forward by 1
+ - This results in the tortoise moving forward by 1 step and the hare by 2 steps
+ - It then compares the sequence values at these two pointers
+ - The smallest value of i > 0 for which the tortoise and hare point to equal values is the desired value ν
+
+ * Pseudocode: 
+ - Declare and initialise tortoise and hare as the head so that they can start traversing the list together
+ - While neither hare nor hare.Next is nil: tortoise = tortoise.Next, hare = hare.Next.Next
+ - If tortoise == hare, break the while loop // In this case return true
+ - This means that a cycle has been found
+ - If tortoise != hare return false
+
+ *Bonus: Identify the beginning of the cycle
+ - In order to identify the node where the cycle begins
+ - From where we broke the loop // if we used break instead of return
+ - Send the tortoise back to the start and make the hare and rabbit move one step at a time each
+ - Where they meet is the node that starts the cycle
+ - Set the tortoise = head
+ - While tortoise != hare: tortoise = tortoise.Next, hare = hare.Next
+ - Return tortoise or hare since they will be at the same place
+
+*/
 type ListNode struct {
 	Val int
 	Next *ListNode
 }
 
-func (head *ListNode) bool {
-	
+
+
+func hasCycle(head *ListNode) bool {
+	// Early return false for nil list
+	if head == nil || head.Next == nil {
+		return false
+	}
+	// Declare tortoise and hare for list traversal
+    tortoise := head
+	hare := head
+
+    for hare != nil && hare.Next != nil {
+        tortoise = tortoise.Next
+		hare = hare.Next.Next
+		if tortoise == hare {
+			return true
+		}
+    }
+	return false
+    
 }
--- 141-linked-list-cycle/main_test.go
+++ 141-linked-list-cycle/main_test.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestHasCycle(t *testing.T) {
+	// Helper function to create a linked list with a cycle
+	createLinkedList := func(values []int, pos int) *ListNode {
+		if len(values) == 0 {
+			return nil
+		}
+
+		dummy := &ListNode{}
+		cur := dummy
+		var cycleNode *ListNode
+
+		for i, val := range values {
+			newNode := &ListNode{Val: val}
+			cur.Next = newNode
+			cur = newNode
+
+			if i == pos {
+				cycleNode = newNode
+			}
+		}
+
+		// Create cycle if pos is valid
+		if cycleNode != nil {
+			cur.Next = cycleNode
+		}
+
+		return dummy.Next
+	}
+
+	tests := []struct {
+		head     *ListNode
+		expected bool
+	}{
+		// Test case 1: input head = [3, 2, 0, -4], pos = 1, output true
+		{createLinkedList([]int{3, 2, 0, -4}, 1), true},
+
+		// Test case 2: input head = [1, 2], pos = 0, output true
+		{createLinkedList([]int{1, 2}, 0), true},
+
+		// Test case 3: input head = [1], pos = -1, output false
+		{createLinkedList([]int{1}, -1), false},
+	}
+
+	for i, test := range tests {
+		result := hasCycle(test.head)
+		if result != test.expected {
+			t.Errorf("Test %d failed: hasCycle() = %v; want: %v", i+1, result, test.expected)
+		}
+	}
+}
