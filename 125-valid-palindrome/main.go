adb8bad82b61d4ad1eb20501ba5365bffe77f3bd
Adam Mwaniki <adamndegwa@protonmail.com>
2025-04-01 14:43:35 +0300

    Completes task

--- 125-valid-palindrome/main.go
+++ 125-valid-palindrome/main.go
@@ -3,32 +3,26 @@
 - Early return true if s is " " i.e. a string with no characters(different from empty string)
 - Strip the string of whitespaces and characters not in the alphabet
 - Join the characters and convert to lowercase to be case insensitive
-- This can be handled using the regexp package
+    - This can be handled using the regexp package
 - A string is a slice of bytes
 - Option 1: Iterate from both ends of the string and compare iterators
-  - for i, j, mid := 0, len(s)-1, int((i+j)/2) ; i < mid && j > mid ; i, j = i+1, j-1 {
-    if s[i] != s[j] {
-    return true
-    } else {
-    return false
+  - i, j  := 0, len(matches)-1
+    for i < j {
+        if matches[i] != matches[j] {
+            return false
+        }
+        i++
+        j--
     }
-    }
-    O(n) time, brought it down from O(n²)
+    O(n) time
 
-- Option 2: split the string in half and compare the values e.g. using reflect.DeepEqual
-  - high := len(s) - 1
-  - low := 0
-  - mid := int((high + low)/2) // force it to round down
-    if !reflect.DeepEqual(s[low:mid], s[mid:high]) { // reflect.DeepEqual is ill advised in production
-    return false
-    } else {
-    return true
-    }
-  - Find a way to improve time complexity
 */
 package main
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 func isPalindrome(s string) bool {
     // Early return true if length of string is 1
@@ -36,18 +30,18 @@ func isPalindrome(s string) bool {
         return true
     } 
 
-    pattern := regexp.MustCompile(`\w`) // regex character class (for alphanumeric + underscore) denoted by \w
-    matches := pattern.FindAllString(s, -1)
+    pattern := regexp.MustCompile(`[a-zA-Z0-9]`) // regex literal for the range of alpha-numeric characters instead of regex class \w to prevent underscore matching
+    matches := pattern.FindAllString(strings.ToLower(s), -1)
 
     i, j  := 0, len(matches)-1
-    mid := int((i+j)/2)
-
-    for  ; i < mid && j > mid ; i, j = i+1, j-1 {
+    for i < j {
         if matches[i] != matches[j] {
-            return true
+            return false
         }
+        i++
+        j--
     }
-    return false
+    return true
 }
 
 func main(){}
